7c4691528862020fdb0b0bd908a23baa5f1dfd14
Tamim Hosen <88259523+tamim404@users.noreply.github.com>
2021-08-30 19:26:05 -0700

    Add files via upload

---
+++ Class1/main.py
@@ -0,0 +1,20 @@
+ 
+
+print("Welcom to 404 store\n")
+
+laptop1 = "401"
+laptop2 = "402"
+laptop3 = "403"
+laptop4 = "404"
+
+print ("Hi Customer build your Laptop !")
+
+model = input("Which Laptop You Want : ")
+ps = input("Enter Your Processor Config : ")
+ram = input("Enter Your Ram size : ")
+hd = input("Enter Your Hard Disk Volume : ")
+
+print ("Hi There\nYou Have Selected Laptop Model "+model+ "\nYour Processor is : "+ps+"\nRam : "+ram+"\nHarddisk : "+hd+"\nPlease Contact Our sales Team for price.\nWe Will Delever Your Laptop ASAP in pandamic situation." )
+
+
+
